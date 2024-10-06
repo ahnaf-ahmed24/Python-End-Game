a74f671758e26213cc657104f76b1b3827dcf298
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-10-06 21:29:31 +0600

    Chapter 6

---
+++ CHAPTER 6/03_multipal_if_statement.py
@@ -0,0 +1,19 @@
+a = int(input("Enter You Number : "))
+
+# a if statement no : 01
+if(a%2 == 0):
+    print("A is even")
+
+# End of if statement no : 01
+
+# if statement no : 02
+if(a>=18):
+    print("You are avobe the age of consent")
+    print("Good for you")
+elif(a<0):
+    print("You are entering an invalide nagetive number")
+else:
+    print("You are below the age of consent")
+# End of i statement no : 02
+
+print("End of program")
