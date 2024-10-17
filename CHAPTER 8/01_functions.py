15de6c043488f1e3322a778fae44cb940fa30af4
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-10-17 12:09:44 +0600

    Chapter 8

---
+++ CHAPTER 8/01_functions.py
@@ -0,0 +1,35 @@
+# a = int(input("Enter Your Number : "))
+# b = int(input("Enter Your Number : "))
+# c = int(input("Enter Your Number : "))
+
+# average = (a + b + c)/3
+# print(average)
+
+
+# a = int(input("Enter Your Number : "))
+# b = int(input("Enter Your Number : "))
+# c = int(input("Enter Your Number : "))
+
+# average = (a + b + c)/3
+# print(average)
+
+# Function Definition
+def avg():
+    a = int(input("Enter Your Number : "))
+    b = int(input("Enter Your Number : "))
+    c = int(input("Enter Your Number : "))
+
+    average = (a + b + c)/3
+    print(average)
+
+# Function call
+avg()
+print('Thanks')
+avg()
+print('Thanks')
+avg()
+print('Thanks')
+avg()
+print('Thanks')
+avg()
+print('Thanks')
---
+++ CHAPTER 8/03_functions_with_arguments.py
@@ -0,0 +1,31 @@
+# def goodDay(name):
+#     print("Good Day, " + name)
+
+#     # or
+#     # print(f"Good Day, {name}")
+# goodDay("Ahnaf")
+# goodDay("Miha")
+# goodDay("Konok")
+
+
+
+
+# def goodDay(name, ending):
+#     print("Good Day, " + name)
+#     print(ending)
+   
+# goodDay("Ahnaf" "Thank you")
+# goodDay("Miha" "Thank you")
+# goodDay("Konok" "Thanks")
+
+
+
+
+
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "Done"
+   
+a = goodDay("Ahnaf" "Thank you")
+print(a)
